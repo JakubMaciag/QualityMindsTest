14f90e4c7c2ee77a67d6fa6a71d7bc41108de136
Jakub Maciąg <jacob.maciag@gmail.com>
2019-12-17 21:00:10 +0100

    test listeners

--- src/test/java/utils/BaseTest.java
+++ src/test/java/utils/BaseTest.java
@@ -1,4 +1,18 @@
 package utils;
 
+import PageObject.BasePage;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.WebDriver;
+
+@Slf4j
 public class BaseTest {
+    public BaseTest() {
+        super();
+    }
+
+    @Getter(AccessLevel.PUBLIC)
+    protected static WebDriver driver;
+
 }
